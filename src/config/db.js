86134f7d3de1db338d418bbdffaed0c022c5fe08
ianoliveirasilva <ianoliveira.tec@gmail.com>
2024-05-01 17:37:30 -0300

    Adding database

--- src/config/db.js
+++ src/config/db.js
@@ -2,8 +2,9 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString:
-    "postgres://postgres:root123@localhost:5432/estantevirtual",
+    "postgres://estantevirtual_user:Sf1DOGDz3HrfwDrAeE0ivIB4hCMQsoRL@dpg-copa34f79t8c73bmb3c0-a.virginia-postgres.render.com:5432/estantevirtual",
 });
+
 
 pool.connect((err) => {
   if (err) {
